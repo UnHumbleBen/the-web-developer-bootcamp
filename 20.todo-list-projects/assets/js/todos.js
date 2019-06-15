869bec15d308ce3a845537304f497c8c22c493f1
Benjamin Lee <bnllee@ucdavis.edu>
2019-06-15 12:41:39 -0700

    removed To-Do elements when X ix clicked

--- 20.todo-list-projects/assets/js/todos.js
+++ 20.todo-list-projects/assets/js/todos.js
@@ -1,4 +1,14 @@
-// Checks off To-Dos when X is clicked.
+// Checks off To-Dos clicked.
 $("li").on("click", function () {
     $(this).toggleClass("completed");
 });
+
+// Deletes To-Do when X ix clicked.
+$("span").on("click", function (event) {
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+    });
+
+    // Prevents event bubbling.
+    event.stopPropagation();
+})
