ff54d24d9328ea10cdb5643ca68a60b364f4604d
Benjamin Lee <bnllee@ucdavis.edu>
2019-06-15 18:30:04 -0700

    can now add new To-Dos

--- 20.todo-list-projects/assets/js/todos.js
+++ 20.todo-list-projects/assets/js/todos.js
@@ -1,14 +1,24 @@
-// Checks off To-Dos clicked.
-$("li").on("click", function () {
+// Listener for checking on/off To-Dos.
+$("ul").on("click", "li", function () {
     $(this).toggleClass("completed");
 });
 
-// Deletes To-Do when X ix clicked.
-$("span").on("click", function (event) {
+// Listener for removing deleted To-Dos.
+$("ul").on("click", "span", function (event) {
     $(this).parent().fadeOut(500, function () {
         $(this).remove();
     });
 
     // Prevents event bubbling.
     event.stopPropagation();
-})
+});
+
+// Listener for adding new To-Dos.
+$("input[type='text']").keypress(function (event) {
+    if (event.which === 13) {
+        // When Enter key is pressed, empties the text bar and append to list.
+        $(this).val("");
+        var todoStr = $(this).val();
+        $("ul").append("<li><span>X</span> " + todoStr + "</li>");
+    }
+});
