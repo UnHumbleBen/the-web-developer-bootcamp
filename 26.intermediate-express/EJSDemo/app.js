321e3e5eb31b13b9e9f29f9efbcb1ca1ae67e933
Benjamin Lee <bnllee@ucdavis.edu>
2019-06-19 16:54:32 -0700

    ejs conditionals and loops

--- 26.intermediate-express/EJSDemo/app.js
+++ 26.intermediate-express/EJSDemo/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 // Handles requests to the home page.
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   res.render('home.ejs');
 });
 
@@ -11,6 +11,16 @@ app.get('/', (req, res) => {
 app.get('/fallinlovewith/:thing', (req, res) => {
   const { thing } = req.params;
   res.render('love.ejs', { thingVar: thing });
+});
+
+app.get('/posts', (_req, res) => {
+  const posts = [
+    { title: 'Post 1', author: 'Susy' },
+    { title: 'My adorable pet bunny', author: 'Charlie' },
+    { title: 'Can you believe this pomsky?', author: 'Colt' },
+  ];
+
+  res.render('posts.ejs', { posts });
 });
 
 app.listen(process.env.PORT, process.env.IP, () => {
