bd07dc2518ae1d6fa675a08bd16a219710dd2bf0
Benjamin Lee <bnllee@ucdavis.edu>
2019-06-19 12:10:29 -0700

    linter

--- 25.server-side-frameworks/FirstExpressApp/app.js
+++ 25.server-side-frameworks/FirstExpressApp/app.js
@@ -1,37 +1,38 @@
-var express = require("express");
-var app = express();
+const express = require('express');
+
+const app = express();
 
 // "/" => "Hi there!"
-app.get("/", function (req, res) {
-  res.send("Hi there!");
+app.get('/', (req, res) => {
+  res.send('Hi there!');
 });
 // "/bye" => "Goodbye!"
-app.get("/bye", function (req, res) {
-  res.send("Goodbye!");
+app.get('/bye', (req, res) => {
+  res.send('Goodbye!');
 });
 // "/dog" => "MEOW!"
-app.get("/dog", function (req, res) {
-  console.log("Someone made a request to /dog");
-  res.send("MEOW!");
+app.get('/dog', (req, res) => {
+  console.log('Someone made a request to /dog');
+  res.send('MEOW!');
 });
 
-app.get("/r/:subredditName", function (req, res) {
-  var subreddit = req.params.subredditName;
-  res.send("WELCOME TO THE " + subreddit.toUpperCase() + " SUBREDDIT!");
+app.get('/r/:subredditName', (req, res) => {
+  const subreddit = req.params.subredditName;
+  res.send(`WELCOME TO THE ${subreddit.toUpperCase()} SUBREDDIT!`);
 });
 
-app.get("/r/:subredditName/comments/:id/:title", function (req, res) {
+app.get('/r/:subredditName/comments/:id/:title', (req, res) => {
   console.log(req.params);
-  res.send("welcome to a comments page!");
+  res.send('welcome to a comments page!');
 });
 
 // This needs to be placed last since the first route that matches the request will be run.
-app.get("*", function (req, res) {
-  res.send("You are a star!");
+app.get('*', (req, res) => {
+  res.send('You are a star!');
 });
 
 
 // Tells Express to listen for request (start server)
-app.listen(process.env.PORT, process.env.IP, function () {
-  console.log("Server has started!");
+app.listen(process.env.PORT, process.env.IP, () => {
+  console.log('Server has started!');
 });
