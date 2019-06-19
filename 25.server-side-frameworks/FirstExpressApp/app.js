932f139ef9ee220a2098c54c561adb025f6e29c8
Benjamin Lee <bnllee@ucdavis.edu>
2019-06-18 23:59:01 -0700

    first express app

--- 25.server-side-frameworks/FirstExpressApp/app.js
+++ 25.server-side-frameworks/FirstExpressApp/app.js
@@ -1,1 +1,21 @@
-console.log("Our express app will run here");
+var express = require("express");
+var app = express();
+
+// "/" => "Hi there!"
+app.get("/", function (req, res) {
+    res.send("Hi there!");
+});
+// "/bye" => "Goodbye!"
+app.get("/bye", function (req, res) {
+    res.send("Goodbye!");
+});
+// "/dog" => "MEOW!"
+app.get("/dog", function (req, res) {
+    console.log("Someone made a request to /dog");
+    res.send("MEOW!");
+});
+
+// Tells Express to listen for request (start server)
+app.listen(process.env.PORT, process.env.IP, function () {
+    console.log("Server has started!");
+});
