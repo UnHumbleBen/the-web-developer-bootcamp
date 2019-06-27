41d411524135d3d9b2b05b7fb106b8ea2d943d63
Benjamin Lee <bnllee@ucdavis.edu>
2019-06-26 22:13:49 -0700

    post requests part 2

--- 26.intermediate-express/PostRequestDemo/app.js
+++ 26.intermediate-express/PostRequestDemo/app.js
@@ -1,25 +1,30 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({"extended": true}));
 
 app.set('view engine', 'ejs');
+const friends = [
+  'Tony',
+  'Miranda',
+  'Justin',
+  'Pierre',
+  'Lily'
+];
 
 app.get('/', (req, res) => {
   res.render('home');
 });
 
 app.post('/addfriend', (req, res) => {
-  res.send('You have reached the post route!');
+  const {newfriend} = req.body;
+
+  friends.push(newfriend);
+  res.redirect('/friends');
 });
 
 app.get('/friends', (req, res) => {
-  const friends = [
-    'Tony',
-    'Miranda',
-    'Justin',
-    'Pierre',
-    'Lily'
-  ];
-
   res.render('friends', {friends});
 });
 
