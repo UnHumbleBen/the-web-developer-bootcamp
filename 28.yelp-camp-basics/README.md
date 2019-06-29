6def191420ee3a1a550df34d33f0c2751f976450
Benjamin Lee <bnllee@ucdavis.edu>
2019-06-28 23:02:59 -0700

    added headers, footers, and bootstrap

--- 28.yelp-camp-basics/README.md
+++ 28.yelp-camp-basics/README.md
@@ -3,6 +3,10 @@
 * Add Loading Page
 * Add Campgrounds Page that lists all campgrounds
 
-Each Campgrouhd has:
+## Each Campgrouhd has:
 * Name
 * Image
+
+# Layout and Basic Styling
+* Create our header and footer partials
+* Add in Bootstrap
--- 28.yelp-camp-basics/app.js
+++ 28.yelp-camp-basics/app.js
@@ -10,8 +10,8 @@ app.get('/', (req, res) => {
 
 app.get('/campgrounds', (req, res) => {
   const campgrounds = [
-    { name: 'Salmon Creek', image: 'https://pixabay.com/get/57e1dd4a4350a514f6da8c7dda793f7f1636dfe2564c704c732e7ddd924fc45a_340.jpg' },
-    { name: 'Granite Hill', image: 'https://pixabay.com/get/57e1d14a4e52ae14f6da8c7dda793f7f1636dfe2564c704c732e7ddd924fc45a_340.jpg' },
+    { name: 'Salmon Creek', image: 'https://www.photosforclass.com/download/pixabay-1149402?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e1d14a4e52ae14f6da8c7dda793f7f1636dfe2564c704c732e7cdc9045c65f_960.jpg&user=Free-Photos' },
+    { name: 'Granite Hill', image: 'https://www.photosforclass.com/download/pixabay-1189929?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e1dd4a4350a514f6da8c7dda793f7f1636dfe2564c704c732e7cdc9045c65f_960.jpg&user=Noel_Bauza' },
     { name: 'Mountain Goat\'s Rest', image: 'https://www.photosforclass.com/download/pixabay-1208201?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e2d54b4852ad14f6da8c7dda793f7f1636dfe2564c704c732e7ddd924fc45a_960.jpg&user=Free-Photos' },
   ];
   res.render('campgrounds', { campgrounds });
