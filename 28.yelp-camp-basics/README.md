46bc078ac0eb4b3807f40f2c75317c217461c43d
Benjamin Lee <bnllee@ucdavis.edu>
2019-06-30 12:43:52 -0700

    added POST route to cretae new campgrounds

--- 28.yelp-camp-basics/README.md
+++ 28.yelp-camp-basics/README.md
@@ -3,10 +3,16 @@
 * Add Loading Page
 * Add Campgrounds Page that lists all campgrounds
 
-## Each Campgrouhd has:
+## Each Campground has:
 * Name
 * Image
 
-# Layout and Basic Styling
+## Layout and Basic Styling
 * Create our header and footer partials
 * Add in Bootstrap
+
+## Creating New Campgrounds
+* Setup new campground POST route
+* Add in body-parser
+* Setup route to show form
+* Add basic unstyled form
--- 28.yelp-camp-basics/app.js
+++ 28.yelp-camp-basics/app.js
@@ -2,21 +2,47 @@ const express = require('express');
 
 const app = express();
 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Specifies the view engine to allow omision of ejs suffix when calling render functions.
 app.set('view engine', 'ejs');
 
+const campgrounds = [
+  { name: 'Salmon Creek', image: 'https://www.photosforclass.com/download/pixabay-1149402?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e1d14a4e52ae14f6da8c7dda793f7f1636dfe2564c704c732e7cdc9045c65f_960.jpg&user=Free-Photos' },
+  { name: 'Granite Hill', image: 'https://www.photosforclass.com/download/pixabay-1189929?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e1dd4a4350a514f6da8c7dda793f7f1636dfe2564c704c732e7cdc9045c65f_960.jpg&user=Noel_Bauza' },
+  { name: 'Mountain Goat\'s Rest', image: 'https://www.photosforclass.com/download/pixabay-1208201?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e2d54b4852ad14f6da8c7dda793f7f1636dfe2564c704c732e7ddd924fc45a_960.jpg&user=Free-Photos' },
+];
+
+// Renders the landinage page.
 app.get('/', (req, res) => {
   res.render('landing');
 });
 
+// Renders the campground page.
 app.get('/campgrounds', (req, res) => {
-  const campgrounds = [
-    { name: 'Salmon Creek', image: 'https://www.photosforclass.com/download/pixabay-1149402?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e1d14a4e52ae14f6da8c7dda793f7f1636dfe2564c704c732e7cdc9045c65f_960.jpg&user=Free-Photos' },
-    { name: 'Granite Hill', image: 'https://www.photosforclass.com/download/pixabay-1189929?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e1dd4a4350a514f6da8c7dda793f7f1636dfe2564c704c732e7cdc9045c65f_960.jpg&user=Noel_Bauza' },
-    { name: 'Mountain Goat\'s Rest', image: 'https://www.photosforclass.com/download/pixabay-1208201?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e2d54b4852ad14f6da8c7dda793f7f1636dfe2564c704c732e7ddd924fc45a_960.jpg&user=Free-Photos' },
-  ];
   res.render('campgrounds', { campgrounds });
 });
 
+// Gets data from form and add it to campgrounds array.
+// Redirects back to campgrounds page.
+app.post('/campgrounds', (req, res) => {
+  const { name } = req.body;
+  const { image } = req.body;
+  const newCampground = { name, image };
+  campgrounds.push(newCampground);
+
+  // Redirect is a GET request.
+  res.redirect('/campgrounds');
+});
+
+// Redirects to the form page.
+app.get('/campgrounds/new', (req, res) => {
+  res.render('new');
+});
+
+// Starts the servert on @PORT.
 app.listen(process.env.PORT, process.env.IP, () => {
   // eslint-disable-next-line no-console
   console.log('The YelpCamp server has started!');
