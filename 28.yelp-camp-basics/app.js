17317c1ba5efb6dd128247066df2b761e65a795c
Benjamin Lee <bnllee@ucdavis.edu>
2019-06-30 21:18:36 -0700

    styled campgrounds with bootstrap

--- 28.yelp-camp-basics/app.js
+++ 28.yelp-camp-basics/app.js
@@ -11,7 +11,10 @@ app.set('view engine', 'ejs');
 
 const campgrounds = [
   { name: 'Salmon Creek', image: 'https://www.photosforclass.com/download/pixabay-1149402?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e1d14a4e52ae14f6da8c7dda793f7f1636dfe2564c704c732e7cdc9045c65f_960.jpg&user=Free-Photos' },
-  { name: 'Granite Hill', image: 'https://www.photosforclass.com/download/pixabay-1189929?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e1dd4a4350a514f6da8c7dda793f7f1636dfe2564c704c732e7cdc9045c65f_960.jpg&user=Noel_Bauza' },
+  { name: 'Granite Hill', image: 'https://www.photosforclass.com/download/pixabay-1867275?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e8d3444855a914f6da8c7dda793f7f1636dfe2564c704c732e72d6954bcd5c_960.jpg&user=Pexels' },
+  { name: 'Mountain Goat\'s Rest', image: 'https://www.photosforclass.com/download/pixabay-1208201?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e2d54b4852ad14f6da8c7dda793f7f1636dfe2564c704c732e7ddd924fc45a_960.jpg&user=Free-Photos' },
+  { name: 'Salmon Creek', image: 'https://www.photosforclass.com/download/pixabay-1149402?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e1d14a4e52ae14f6da8c7dda793f7f1636dfe2564c704c732e7cdc9045c65f_960.jpg&user=Free-Photos' },
+  { name: 'Granite Hill', image: 'https://www.photosforclass.com/download/pixabay-1867275?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e8d3444855a914f6da8c7dda793f7f1636dfe2564c704c732e72d6954bcd5c_960.jpg&user=Pexels' },
   { name: 'Mountain Goat\'s Rest', image: 'https://www.photosforclass.com/download/pixabay-1208201?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F57e2d54b4852ad14f6da8c7dda793f7f1636dfe2564c704c732e7ddd924fc45a_960.jpg&user=Free-Photos' },
 ];
 
